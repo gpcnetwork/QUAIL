74a7c03e84cbd223fecd2eb7bf7d681872a75f2c
Yahia Mohamed <77698104+alabidi21@users.noreply.github.com>
2022-02-19 22:25:05 -0600

    Update ETL_issues check.sql

--- DQ_rules/Source_code/ETL_issues check.sql
+++ DQ_rules/Source_code/ETL_issues check.sql
@@ -1,1 +1,23 @@
+/**Check for ETL issues in the PCORnet database by tracking gentamicin medication and order of creatinine lab test for inpatient encounters**/  
+
+
+---Check using 2021 cyle
+
+SELECT pp.PRESCRIBINGID, pp.* 
+FROM PCORNET_CDM.CDM_C009R020.ENCOUNTER pe
+LEFT JOIN PCORNET_CDM.CDM_C009R020.PRESCRIBING pp ON pp.encounterid = pe.encounterid
+WHERE pe.ENC_TYPE= 'IP' 
+AND pp.RAW_RX_MED_NAME LIKE 'gentamicin%' 
+AND pe.ADMIT_DATE BETWEEN '2020-1-1' AND '2020-2-1' AND pp.encounterid = *******
+ORDER BY  pp.RX_ORDER_DATE, pp.RX_ORDER_Time, pp.Rx_start_date;
+
+---Check using 2022 after fixing the ETL glitch
+
+SELECT pp.PRESCRIBINGID, pp.* 
+FROM PCORNET_CDM.CDM_C010R022.ENCOUNTER pe
+LEFT JOIN PCORNET_CDM.CDM_C010R022.PRESCRIBING pp ON pp.encounterid = pe.encounterid
+WHERE pe.ENC_TYPE= 'IP' 
+AND pp.RAW_RX_MED_NAME LIKE 'gentamicin%' 
+AND pe.ADMIT_DATE BETWEEN '2020-1-1' AND '2020-2-1' AND pp.encounterid =******* 
+ORDER BY  pp.RX_ORDER_DATE, pp.RX_ORDER_Time, pp.Rx_start_date;
 
